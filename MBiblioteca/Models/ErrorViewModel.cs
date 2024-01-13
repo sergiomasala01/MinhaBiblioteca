7ae5fce2e3eeced71f74d113480e5e9bf1717344
Sérgio <Srgio@DESKTOP-TN2NN4A>
2024-01-13 19:25:23 -0300

    Adicionar arquivos de projeto.

---
+++ MBiblioteca/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MBiblioteca.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
